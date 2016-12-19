d7ff4720fc2de88cb2fd9c0fe7db64ea2ff59558
thomas <thomas@sonicmoov.com>
2016-12-19 18:16:18 +0900

    test and debug and develop 2.0.0

--- tests/MetaDataTest.php
+++ tests/MetaDataTest.php
@@ -9,23 +9,20 @@ class MetaDataTest extends \PHPUnit_Framework_TestCase
 {
 
     /**
-     * @var \Phalcon\Di\FactoryDefault
-     */
-    protected $di = null;
-
-    /**
      * set up
      */
     public function setUp()
     {
         // FactoryDefault
-        $this->di = new Phalcon\Di\FactoryDefault();
+        $di = new Phalcon\Di\FactoryDefault();
 
         // config
         $config = new \Phalcon\Config();
         $yml    = new \Phalcon\Config\Adapter\Yaml(__DIR__ . "/redis.yml");
         $config->merge($yml->get("test"));
-        $this->di->set("config", function () use ($config) { return $config; }, true);
+        $di->set("config", function () use ($config) { return $config; }, true);
+
+        \Phalcon\DI::setDefault($di);
     }
 
     /**
@@ -33,10 +30,11 @@ class MetaDataTest extends \PHPUnit_Framework_TestCase
      */
     public function testModelsMetadata()
     {
-        $di = $this->di;
+        $di = \Phalcon\DI::getDefault();
         $di->setShared("modelsMetadata", function () use ($di) {
             return new Redis($di->get("redis")->get("metadata")->toArray());
         });
+        \Phalcon\DI::setDefault($di);
 
         $model   = new MstItem();
         $indexes = $model->getModelsMetadata()->readIndexes($model);
