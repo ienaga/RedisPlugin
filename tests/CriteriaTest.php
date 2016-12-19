6cb02bc03e951845d1793e65427b14a7642c661a
thomas <thomas@sonicmoov.com>
2016-12-19 16:18:44 +0900

    test and debug and develop 2.0.0

--- tests/CriteriaTest.php
+++ tests/CriteriaTest.php
@@ -1,12 +1,37 @@
 <?php
 
 require_once __DIR__ . "/../src/mvc/model/Criteria.php";
+require_once __DIR__ . "/../src/plugin/redis/Service.php";
 require_once __DIR__ . "/MstItem.php";
-
-use \RedisPlugin\Mvc\Model\Criteria;
 
 class CriteriaTest extends \PHPUnit_Framework_TestCase
 {
+
+    /**
+     * set up
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        // config
+        $config = new \Phalcon\Config();
+        $yml    = new \Phalcon\Config\Adapter\Yaml(__DIR__ . "/redis.yml");
+        $config->merge($yml->get("test"));
+
+        $di = new Phalcon\Di\FactoryDefault();
+        $di->set("config", function () use ($config) { return $config; }, true);
+
+        $dbService = new \RedisPlugin\Service();
+        $dbService->registration();
+
+        $di->setShared("modelsMetadata", function () {
+            return new \RedisPlugin\Mvc\Model\Metadata\Redis(
+                $this->getConfig()->get("redis")->get("metadata")->toArray()
+            );
+        });
+    }
+
     /**
      * test find first
      */
