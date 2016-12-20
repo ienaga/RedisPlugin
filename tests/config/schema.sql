17006a62edd6da3c4ddc143ee526641aae9f6073
thomas <thomas@sonicmoov.com>
2016-12-20 13:56:26 +0900

    test and debug and develop 2.0.0

--- tests/config/schema.sql
+++ tests/config/schema.sql
@@ -3,31 +3,28 @@ CREATE DATABASE IF NOT EXISTS `common`;
 CREATE DATABASE IF NOT EXISTS `user1`;
 CREATE DATABASE IF NOT EXISTS `user2`;
 
-USE `admin`;
 
-CREATE TABLE IF NOT EXISTS `admin_user` (
+
+CREATE TABLE IF NOT EXISTS `admin`.`admin_user` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `admin_config_db_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-
-CREATE TABLE IF NOT EXISTS `admin_config_db` (
+CREATE TABLE IF NOT EXISTS `admin`.`admin_config_db` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `gravity` tinyint(3) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-INSERT INTO `admin_config_db` (`id`, `name`, `gravity`) VALUES
+INSERT INTO `admin`.`admin_config_db` (`id`, `name`, `gravity`) VALUES
 (1, 'dbUser1', 50),
 (2, 'dbUser2', 50);
 
 
 
-USE `common`;
-
-CREATE TABLE IF NOT EXISTS `mst_item` (
+CREATE TABLE IF NOT EXISTS `common`.`mst_item` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `level` tinyint(3) unsigned NOT NULL,
@@ -35,10 +32,10 @@ CREATE TABLE IF NOT EXISTS `mst_item` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-ALTER TABLE `mst_item`
+ALTER TABLE `common`.`mst_item`
   ADD KEY `level_mode_idx` (`level`, `mode`) USING BTREE;
 
-INSERT INTO `mst_item` (`id`, `name`, `level`, `mode`) VALUES
+INSERT INTO `common`.`mst_item` (`id`, `name`, `level`, `mode`) VALUES
   (1, 'item_1', 1, 1),
   (2, 'item_2', 2, 1),
   (3, 'item_3', 1, 1),
@@ -47,21 +44,37 @@ INSERT INTO `mst_item` (`id`, `name`, `level`, `mode`) VALUES
   (6, 'item_6', 2, 2);
 
 
-USE `user1`;
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE IF NOT EXISTS `user1`.`user` (
   `id` int(10) unsigned NOT NULL,
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS `user1`.`user_item` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `item_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+ALTER TABLE `user1`.`user_item`
+  ADD KEY `user_id_idx` (`user_id`) USING BTREE;
 
 
 
-USE `user2`;
-
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE IF NOT EXISTS `user2`.`user` (
   `id` int(10) unsigned NOT NULL,
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `user2`.`user_item` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `item_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+ALTER TABLE `user2`.`user_item`
+  ADD KEY `user_id_idx` (`user_id`) USING BTREE;
