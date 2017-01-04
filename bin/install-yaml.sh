4a6cd3d2c16e0ec55f23d8c9cca9b6a4941aa13a
thomas <thomas@sonicmoov.com>
2017-01-04 11:52:33 +0900

    edit travis.yml and add sh

---
+++ bin/install-yaml.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+START_TIME=$(date +%s.%3N)
+
+
+# Ensure that this is being run inside a CI container
+if [ "${CI}" != "true" ]; then
+    echo "This script is designed to run inside a CI container only. Exiting"
+    exit 1
+fi
+
+PHP_VER=$(phpenv version-name)
+
+if [[ $PHP_VER == 7* ]]; then
+    pecl install -f yaml-2.0.0  < /dev/null
+else
+    pecl install -f yaml  < /dev/null
+fi
